0a1d6eb489aa9cfa93391869dee8e9ba95896bf0
Kelvin <kelvin.martinez92@gmail.com>
2020-01-04 11:57:04 -0500

    updated javascript

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
 
+
     //this will update the button we have on the page//
     $("#searchbtn").on("click", function(e){
 
@@ -10,49 +11,74 @@ $(document).ready(function(){
         var query = $("#searchHere").val();
 
         //this is where the API link will go - make sure you change it and add the value taken from the input and concatinate it to the API//
-        var url = "";
 
+        var url = "https://api-football-v1.p.rapidapi.com/v2/teams/search/"+ query +"&apiKey=b14a035665msh3cee34533db4371p18b45fjsn6f4ad8ee9190";
+       
+       
         if(query !== ""){
 
-            $.ajax({
-                //this url will carry the API from above
-                url:url,
-                method:"GET",
-                dataType: "Json", 
-                //below i need to set variables for the different arrays within the API that we are pulling like the stats or the latest news//
-                success: function(stats){
-                    var output = "";
-                    var latestNews = stats.articles;
-                    //below we will update it with the documentation within the API which will show in the browser in the specific DIV assigned to it//
-                    for(var i in latestNews){
-                        output +=`
-                        <h4>${latestNews[i].title}</h4>
-                        <img src="${latestNews[i].urlToImage}>
-                        <p>${latestNews[i].description}</p>
-                        <p>${latestNews[i].content}</p>
-                        `;
 
-                    }
+            var input = document.getElementById("myInput");
+            input.addEventListener("keyup", function(event) {
+              if (event.keyCode === "enter") {
+               event.preventDefault();
+               document.getElementById("searchButton").click();
+                
+                showPage();
 
-                    if(output !== ""){
+              }
 
-                    }
-                    else{
-                        $("#searchResults").html("There is no news");
-
-                    }
-                } 
-
-
-            })
-
-        }
-        else{
-
-            console.log("Please Enter Something");
-        }
-
-
+            });
+    
+            function showPage(){
+                $.ajax({
+                    //this url will carry the API from above
+                    url:url,
+                    method:"GET",
+                    dataType: "Json", 
+                    //below i need to set variables for the different arrays within the API that we are pulling like the stats or the latest news//
+                    success: function(stats){
+                        var output = "";
+                        var latestNews = stats.articles;
+                        //below we will update it with the documentation within the API which will show in the browser in the specific DIV assigned to it//
+                        for(var i in latestNews){
+                            output +=`
+                            //this is where we need to update the DOM with paragraphs tags to show the information from the API//
+                            `;
+    
+                        }
+    
+                        if(output !== ""){
+                            //below is what will show the results if statistics are found and display them in the browser//
+                            $("#searchResults").html(output);
+    
+                        }
+    
+                        //below is what will show if no information or data is found for what was input by the user//
+                        else{
+    
+                            var newsNotFound = "There is no news available!"
+                            $("#searchResults").html(newsNotFound);
+    
+                        }
+                    } 
+    
+    
+                })
+    
+            }
+            else{
+    
+                console.log("Please Enter Something");
+            }
+    
+    
+        });
+    
     });
 
-});
+
+
+
+            }
+           
